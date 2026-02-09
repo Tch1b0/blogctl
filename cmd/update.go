ceca5a9fd81677c61b82f1adc5d7b461a841850c
Raphael Pour <info@raphaelpour.de>
2026-02-09 11:01:23 +0100

    Update update.go

--- cmd/update.go
+++ cmd/update.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 Raphael Pour <raphael.pour@hetzner.com>
+Copyright © 2020 Raphael Pour <info@raphael.pour>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
